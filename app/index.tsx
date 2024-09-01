3d11c970bf57e1e5e5b25a7c27b7dfe53f7b1584
Abhik Baidya <abhikbaidya7@gmail.com>
2024-09-01 21:38:41 +0530

    app commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,27 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
-import welcomeImage from '@/assets/images/welcome.png';
-import Colors from '@/constants/Colors';
-import { Link } from 'expo-router';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import welcomeImage from "@/assets/images/welcome.png";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
 const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
 
 const page = () => {
   const openLink = () => {
-    Linking.openURL('https://github.com/07HypeR');
+    Linking.openURL("https://github.com/07HypeR");
   };
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: welcome_image}} style={styles.welcome}/>
+      <Image source={{ uri: welcome_image }} style={styles.welcome} />
       <Text style={styles.headline}>Welcome to WhatsApp Clone</Text>
       <Text style={styles.description}>
-        Read our{' '}
+        Read our{" "}
         <Text style={styles.link} onPress={openLink}>
           Privacy Policy
         </Text>
@@ -26,48 +33,48 @@ const page = () => {
       </Text>
       <Link href={"/otp"} replace asChild>
         <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Agree & Continue</Text>
+          <Text style={styles.buttonText}>Agree & Continue</Text>
         </TouchableOpacity>
       </Link>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   welcome: {
-    width: '100%',
+    width: "100%",
     height: 300,
     marginBottom: 80,
   },
   headline: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginVertical: 20,
   },
   description: {
     fontSize: 14,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 80,
-    color: Colors.gray
+    color: Colors.gray,
   },
   link: {
-    color : Colors.primary
+    color: Colors.primary,
   },
   button: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
     fontSize: 22,
     color: Colors.primary,
-    fontWeight: 'bold',
-  }
-})
-export default page
+    fontWeight: "bold",
+  },
+});
+export default page;
